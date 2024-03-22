e4bb23b21a9c55b4fa8a6ad316445a4a9a32a4ec
Valentin Ackva <74772523+vackva@users.noreply.github.com>
2024-03-22 15:36:30 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,15 +7,23 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
+
+  // Set the base path and asset prefix to match the GitHub Pages subpath
+  basePath: '/nextjs-website-test',
+  assetPrefix: '/nextjs-website-test/',
+
+  // Optional configurations:
+  // Enable trailing slashes in URLs. 
+  // This is a personal or project-specific preference.
+  trailingSlash: true,
  
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
+  // If you want to disable automatic redirection from `/me` to `/me/`, 
+  // you can enable this option. This is also a preference.
+  skipTrailingSlashRedirect: true,
  
-  // Optional: Change the output directory `out` -> `dist`
+  // If you wish to change the output directory from `out` to something else,
+  // you can specify it here. This might be useful for specific deployment configurations.
   // distDir: 'dist',
 }
- 
+
 module.exports = nextConfig
