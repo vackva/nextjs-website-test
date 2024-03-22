2ae42a2960ce0a91005e9e08a56d7417124223fc
Valentin Ackva <valentin.ackva@insone.de>
2024-03-22 16:00:04 +0100

    moved files

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -3,19 +3,14 @@ import { CMS_NAME } from "@/lib/constants";
 export function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
-      </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{" "}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-blue-600 duration-200 transition-colors"
-        >
-          Next.js
-        </a>{" "}
-        and {CMS_NAME}.
-      </h4>
+      <div>
+        <h1 className="text-4xl md:text-6xl font-bold tracking-tighter leading-tight md:pr-8">
+          nn-inference-template
+        </h1>
+        <h2 className="text-2xl md:text-4xl font-light tracking-tight mt-4 md:mt-2">
+          ADC23 Talk: Real-time inference of neural networks - a practical approach for DSP engineers
+        </h2>
+      </div>
     </section>
   );
 }
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,15 +3,16 @@ import fs from "fs";
 import matter from "gray-matter";
 import { join } from "path";
 
-const postsDirectory = join(process.cwd(), "_posts");
+const postsDirectory = join(process.cwd(), "_posts/stories");
+const aboutUsDirectory = join(process.cwd(), "_posts/about-us");
 
-export function getPostSlugs() {
-  return fs.readdirSync(postsDirectory);
+export function getPostSlugs(directory = postsDirectory) {
+  return fs.readdirSync(directory);
 }
 
-export function getPostBySlug(slug: string) {
+export function getPostBySlug(slug: string, directory = postsDirectory) {
   const realSlug = slug.replace(/\.md$/, "");
-  const fullPath = join(postsDirectory, `${realSlug}.md`);
+  const fullPath = join(directory, `${realSlug}.md`);
   const fileContents = fs.readFileSync(fullPath, "utf8");
   const { data, content } = matter(fileContents);
 
@@ -23,6 +24,17 @@ export function getAllPosts(): Post[] {
   const posts = slugs
     .map((slug) => getPostBySlug(slug))
     // sort posts by date in descending order
+    .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
+  return posts;
+}
+
+// New function to get all posts from the about-us directory
+export function getAboutUs(): Post[] {
+  const slugs = getPostSlugs(aboutUsDirectory);
+  const posts = slugs
+    .map((slug) => getPostBySlug(slug, aboutUsDirectory))
+    // You might want to adjust the sorting if these posts don't have a date
+    // For example, you could sort them alphabetically by slug if there's no date
     .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
   return posts;
 }
